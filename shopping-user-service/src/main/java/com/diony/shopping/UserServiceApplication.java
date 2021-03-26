5fcd13b9098fbf0cb57f8e6ca769afc88bafd8f4
v_chenhuan <v_HuanChen@budweiserapac.com>
2021-03-26 17:57:35 +0800

    测试

---
+++ shopping-user-service/src/main/java/com/diony/shopping/UserServiceApplication.java
@@ -0,0 +1,21 @@
+package com.diony.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Nacos 服务端启动类
+ * @Author diony_chen
+ * @Date 2021/3/25
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
+
